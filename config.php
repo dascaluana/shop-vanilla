0e2222d93d6f698252dc519c5afaffdbe9b14992
dascaluana <45557986+dascaluana@users.noreply.github.com>
2018-12-07 11:25:16 +0200

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "shop";
-$adminEmail = "admin@yahoo.com";
-$usernameAdmin = 'username';
-$passwordAdmin = 'password';
