b1cc412ad5458d2fd88ffee39d81a5dd3569e8f4
ArseneAndreea <44430248+ArseneAndreea@users.noreply.github.com>
2018-12-11 18:08:42 +0200

    - DB setup - View - Eloquent 101 - Controller - Layouts and Structure - Form Request Data

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return view('posts.index');
+    }
+
+    public function show()
+    {
+        return view('posts.show');
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store()
+    {
+        // Create a new post using the request data
+        $post = new Post;
+
+        $post->title = request('title');
+        $post->body = request('body');
+
+        // Save it to the DB
+        $post->save();
+
+        // Add them redirect to the home page
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,30 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+ * posts
+ *
+ * GET /posts
+ *
+ * GET /posts/create
+ *
+ * POST /posts
+ *
+ * GET /post/{id}/edit
+ *
+ * GET /posts/{id}
+ *
+ * PATCH /posts/{id}
+ *
+ * DELETE /post/{id}
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PostController@index');
+
+Route::get('/posts/create', 'PostController@create');
+
+Route::post('/posts', 'PostController@store');
+
+//Route::get('/posts/{post}', 'PostController@show');
+
+
+
